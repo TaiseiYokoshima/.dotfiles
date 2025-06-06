75255dd5449de1834f208661758037317e488ab7
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-06-06 11:47:36 +0900

    updated flake

--- flake.nix
+++ flake.nix
@@ -2,22 +2,20 @@
   description = "My dotfiles";
 
   outputs = { self }: {
-    dotfiles = {
-      alacritty = ./alacritty;
-      dunst = ./dunst;
-      fcitx5 = ./fcitx5;
-      fish = ./fish;
-      hypr = ./hypr;
-      kitty = ./kitty;
-      nvim = ./nvim;
-      rofi = ./rofi;
-      scripts = ./scripts;
-      sway = ./sway;
-      tmux = ./tmux;
-      wallpapers = ./wallpapers;
-      waybar = ./waybar;
-      wezterm = ./wezterm;
-      zellij = ./zellij;
-    };
+    alacritty = ./alacritty;
+    dunst = ./dunst;
+    fcitx5 = ./fcitx5;
+    fish = ./fish;
+    hypr = ./hypr;
+    kitty = ./kitty;
+    nvim = ./nvim;
+    rofi = ./rofi;
+    scripts = ./scripts;
+    sway = ./sway;
+    tmux = ./tmux;
+    wallpapers = ./wallpapers;
+    waybar = ./waybar;
+    wezterm = ./wezterm;
+    zellij = ./zellij;
   };
 }
